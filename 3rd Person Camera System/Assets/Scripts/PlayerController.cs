c5f800b87f2d6bb38e7bb999bcbe25df265bbe3b
Kim Long Vo <40185806+tachikoma-gg@users.noreply.github.com>
2024-08-09 00:12:34 -0700

    basic movement complete

--- 3rd Person Camera System/Assets/Scripts/PlayerController.cs
+++ 3rd Person Camera System/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController characterController;
+    private float currentAngle = 0;
+
+    private Vector3 input;
+    private Quaternion currentRotation;
+
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        RotatePlayer();
+        MovePlayer();
+    }
+
+    void RotatePlayer()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
+
+        input = input.magnitude > 1 ? input.normalized : input;
+
+        float targetAngle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg;
+        currentAngle = targetAngle + (currentAngle - targetAngle) * Mathf.Pow(rotationSpeed, Time.deltaTime);
+
+        currentRotation = Quaternion.Euler(0, currentAngle, 0);
+
+        transform.rotation = currentRotation;
+
+        // fix negative angles issue.
+    }
+
+    void MovePlayer()
+    {
+        characterController.Move(currentRotation * Vector3.forward * input.magnitude * movementSpeed * Time.deltaTime);
+
+        // lerp speed?
     }
 }
